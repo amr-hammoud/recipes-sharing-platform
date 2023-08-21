2fb1edff15b3b1937366af0403dbfcde318ea669
Amr Hammoud <amrhammoud.dev@gmail.com>
2023-08-21 08:57:36 +0300

    <BE> added multiple accessors to recipe model

--- backend/app/Models/Recipe.php
+++ backend/app/Models/Recipe.php
@@ -9,36 +9,62 @@ class Recipe extends Model
 {
     use HasFactory;
 
-    public function user(){
+    protected $hidden = ['user','cuisine', 'cuisine_id'];
+    
+    protected $appends = ['cuisine_name','user_username','user_full_name'];
+
+    public function user()
+    {
         return $this->belongsTo(User::class, 'user_id');
     }
 
-    public function likes(){
+    public function likes()
+    {
         return $this->hasMany(Like::class, 'recipe_id');
     }
 
-    public function comments(){
+    public function comments()
+    {
         return $this->hasMany(Comment::class, 'recipe_id');
     }
 
-    public function images(){
+    public function images()
+    {
         return $this->hasMany(Image::class, 'recipe_id');
     }
 
-    public function ingredients(){
+    public function ingredients()
+    {
         return $this->hasMany(IngredientList::class, 'recipe_id');
     }
 
-    public function cuisine(){
+    public function cuisine()
+    {
         return $this->belongsTo(Cuisine::class, 'cuisine_id');
     }
 
-    public function shoppingList(){
+    public function shoppingList()
+    {
         return $this->belongsToMany(ShoppingList::class, 'recipe_id');
     }
 
-    public function mealPlans(){
+    public function mealPlans()
+    {
         return $this->hasMany(MealPlan::class, 'recipe_id');
     }
 
+    public function getCuisineNameAttribute()
+    {
+        return $this->cuisine->name;
+    }
+
+    public function getUserUsernameAttribute()
+    {
+        return $this->user->username;
+    }
+
+    public function getUserFullnameAttribute()
+    {
+        return $this->user->full_name;
+    }
 }
