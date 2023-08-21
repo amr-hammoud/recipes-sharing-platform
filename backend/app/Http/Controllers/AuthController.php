2f994988b2cb97312459fae9ef3b4ba53569d96d
Amr Hammoud <amrhammoud.dev@gmail.com>
2023-08-21 05:59:11 +0300

    <BE> install JWT

---
+++ backend/app/Http/Controllers/AuthController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function unauthorized(Request $request)
+    {
+        return response()->json([
+            'status' => 'Error',
+            'message' => 'Unauthorized',
+        ], 200);
+    }
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'string|email|max:255|nullable',
+            'username' => 'string|max:255|nullable',
+            'password' => 'required|string|min:6',
+        ]);
+
+        $request->email ? $credentials = $request->only('email', 'password') : $credentials = $request->only('username', 'password');
+
+        $token = Auth::attempt($credentials);
+
+        if (!$token) {
+            return response()->json([
+                'status' => 'Error',
+                'message' => 'Unauthorized',
+            ], 401);
+        }
+
+        $user = Auth::user();
+        $user->token = $token;
+
+        return response()->json([
+            'status' => 'Success',
+            'data' => $user
+        ]);
+    }
+
+    public function register(Request $request)
+    {
+        $request->validate([
+            'first_name' => 'required|string|max:255',
+            'last_name' => 'required|string|max:255',
+            'username' => 'required|string|max:255|unique:users',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6',
+        ]);
+
+        $user = new User();
+        $user->first_name = $request->first_name;
+        $user->last_name = $request->last_name;
+        $user->username = $request->username;
+        $user->email = $request->email;
+        $user->password = Hash::make($request->password);
+        $user->save();
+
+        $token = Auth::login($user);
+        $user->token = $token;
+
+        return response()->json([
+            'status' => 'Success',
+            'data' => $user
+        ]);
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Successfully logged out',
+        ]);
+    }
+
+    public function refresh()
+    {
+        $user = Auth::user();
+        $user->token = Auth::refresh();
+
+        return response()->json([
+            'status' => 'success',
+            'data' => $user
+        ]);
+    }
+}
--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -7,8 +7,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use PHPOpenSourceSaver\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use HasApiTokens, HasFactory, Notifiable;
 
@@ -62,5 +63,14 @@ class User extends Authenticatable
 
     public function MealPlan() {
         return $this->hasMany(MealPlan::class, 'user_id');
+    }
+
+    
+    public function getJWTIdentifier(){
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims() {
+        return [];
     }
 }
