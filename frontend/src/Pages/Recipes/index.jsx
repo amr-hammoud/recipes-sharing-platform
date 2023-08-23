e6edb99752bbb0b5cb54b4f51690c9eda1533e2e
Amr Hammoud <amrhammoud.dev@gmail.com>
2023-08-23 12:54:14 +0300

    <FE> added infinite scrolling to recipes

--- frontend/src/Pages/Recipes/index.jsx
+++ frontend/src/Pages/Recipes/index.jsx
@@ -3,35 +3,54 @@ import "./style.css";
 import Navbar from "../../Components/Common/Navbar";
 import RecipeCard from "../../Components/RecipeCard";
 import { sendRequest } from "../../config/request";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 const Recipes = () => {
 	const [recipes, setRecipes] = useState({
-		page: 3,
+		page: 1,
 		list: [],
 	});
 
-	const fetchRecipes = async () => {
+	const [isLoading, setIsLoading] = useState(false)
 
+	const fetchRecipes = async () => {
+		console.log(recipes);
 		const body = {
-			page: recipes.page
-		}
+			page: recipes.page,
+		};
 
 		try {
+			setIsLoading(true);
 			const response = await sendRequest({
 				method: "POST",
 				route: "/recipe",
 				body,
 			});
 
-			setRecipes({ ...recipes, list: response.data });
+			if (recipes.page > 1) {
+				setRecipes((prevRecipes) => ({
+					...prevRecipes,
+					list: [...prevRecipes.list, ...response.data],
+					page: prevRecipes.page + 1,
+				}));
+			} else {
+				setRecipes((prevRecipes) => ({
+					...prevRecipes,
+					list: response.data,
+					page: prevRecipes.page + 1,
+				}));
+			}
+			console.log(recipes);
+			
 		} catch (e) {
 			console.log(e);
+			setIsLoading(false);
+		}finally{
+			setIsLoading(false);
+			console.log(recipes);
 		}
 	};
 
-	useEffect(() => {
-		fetchRecipes();
-	}, []);
 
 	return (
 		<div>
@@ -41,11 +60,20 @@ const Recipes = () => {
 			/>
 			<div className="navbar-page">
 				<div className="container">
-					<div className="recipes-container">
+					<InfiniteScroll
+						dataLength={recipes.list.length}
+						next={fetchRecipes}
+						hasMore={true}
+					>
 						{recipes.list.map((recipe, index) => {
 							return <RecipeCard key={index} recipe={recipe} />;
 						})}
-					</div>
+					</InfiniteScroll>
+					{isLoading && (
+						<div className="flex center">
+							<div className="spinner"></div>
+						</div>
+					)}
 				</div>
 			</div>
 		</div>
