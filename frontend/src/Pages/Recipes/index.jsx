c877ca068a70e050c9dd0dd64b1b27d1560f4c12
Amr Hammoud <amrhammoud.dev@gmail.com>
2023-08-23 07:59:14 +0300

    <FE> setup recipes page

--- frontend/src/Pages/Recipes/index.jsx
+++ frontend/src/Pages/Recipes/index.jsx
@@ -1,13 +1,54 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import "./style.css";
 import Navbar from "../../Components/Common/Navbar";
+import RecipeCard from "../../Components/RecipeCard";
+import { sendRequest } from "../../config/request";
 
 const Recipes = () => {
+	const [recipes, setRecipes] = useState({
+		page: 3,
+		list: [],
+	});
+
+	const fetchRecipes = async () => {
+
+		const body = {
+			page: recipes.page
+		}
+
+		try {
+			console.log(recipes.page);
+			const response = await sendRequest({
+				method: "POST",
+				route: "/recipe",
+				body,
+			});
+
+			setRecipes({ ...recipes, list: response.data });
+		} catch (e) {
+			console.log(e);
+		}
+	};
+
+	useEffect(() => {
+		fetchRecipes();
+	}, []);
+
 	return (
 		<div>
 			<Navbar
 				items={["Recipes", "Shopping List", "Calendar"]}
 				selected={"Recipes"}
 			/>
+			<div className="navbar-page">
+				<div className="container">
+					<div className="recipes-container">
+						{recipes.list.map((recipe) => {
+							return <RecipeCard recipe={recipe} />;
+						})}
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
