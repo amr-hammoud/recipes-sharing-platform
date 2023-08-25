bf492e98982e40c910b292858f0f586eadaed4c9
Amr Hammoud <amrhammoud.dev@gmail.com>
2023-08-25 07:56:43 +0300

    <FE> created search component

--- frontend/src/Pages/Search/index.jsx
+++ frontend/src/Pages/Search/index.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import Navbar from "../../Components/Common/Navbar";
+import Input from "../../Components/Base/Input";
 
 const Search = () => {
-  return (
-    <div>Search</div>
-  )
-}
+	return (
+		<div className="navbar-page light-bg">
+			<Navbar items={["Recipes", "Shopping List", "Calendar"]} />
+			<div className="container white-bg create-recipe">
+				<div className="flex center color-primary mt-20">
+					<h1>Search Recipes</h1>
+				</div>
+				<Input />
+			</div>
+		</div>
+	);
+};
 
-export default Search
+export default Search;
